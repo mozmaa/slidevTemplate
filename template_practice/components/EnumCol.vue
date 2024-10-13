9ab9ef6838232befbacb0ee884b4d3fc48b4e7d8
mozmaa <momirgajicjwd@gmail.com>
2024-10-13 21:01:08 +0200

    Slide 1 and 2

---
+++ template_practice/components/EnumCol.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+    uniqueClass: {
+    type: String,
+    default: ''
+  },
+  icon: {
+    type: String,
+    default: ''
+  },
+  title: {
+    type: String,
+    default: ''
+  },
+  paragraphText: {
+    type: String,
+    default: ''
+  }
+});
+</script>
+
+<template>
+  <div :class="['flex-1 h-screen text-white border-r border-gray-800', uniqueClass]">
+    <h3 class="pt-10 text-center"> {{ icon }}</h3>
+    <h3 class="text-center">{{ title }}</h3>
+    <p class="text-center p-4">{{ paragraphText }}</p>
+  </div>
+</template>
