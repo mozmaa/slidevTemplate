a7dbcde6c7079000e6f8caf85919aca2578d9e0d
mozmaa <momirgajicjwd@gmail.com>
2024-10-19 22:59:42 +0200

    Ende

---
+++ components/EnumerationLine.vue
@@ -0,0 +1,23 @@
+<script setup>
+import '../css/slide.css'
+import { sharedString } from '../snippets/shared-strings';
+
+
+const props = defineProps({
+  uniqueClass: {
+    type: String,
+    default: ''
+  },
+  lineText: {
+    type: String,
+    default: ''
+  },
+  });
+</script>
+
+<template>
+  <div class="mt-10 ml-10 flex gap-6 items-center">
+    <div class="triangle-right"></div>
+    <p class="text-3xl text-white">{{ lineText }}</p>
+  </div>
+</template>
---
+++ components/Logo.vue
@@ -0,0 +1,44 @@
+<script setup>
+import '../css/slide.css'
+import { sharedString } from '../snippets/shared-strings';
+import logoImage from '../assets/DevelopersLogo.png';
+
+
+const props = defineProps({
+  uniqueClass: {
+    type: String,
+    default: ''
+  },
+  title: {
+    type: String,
+    default: sharedString.title
+  },
+  logo: {
+    type: String,
+    default: logoImage
+  },
+  });
+</script>
+
+<!-- <template>
+    <div class="w-full h-24 customColor flex items-center justify-between ">
+      <div class="flex flex-col items-start justify-center ">
+      <h1 :class= "['pl-4 gradient-black-white ', uniqueClass ]">{{ title }}</h1>
+      <p class="pl-4 customP">{{ paragraphText }}</p>
+      </div>
+      <img class="w-44 h-12 pr-4"
+     src="../assets/DevelopersLogoWhite.png" alt="Logo" />
+  </div>
+  
+</template> -->
+
+<template>
+  <div class="w-full h-24 bg-transparent flex items-center justify-between absolute ">
+   <div class="paper-box items-center mx-5 w-full ">
+        <h1 :class= "['pl-4 text-black h1-margin-zero', uniqueClass ]">{{ title }}</h1>
+        <!-- <p class="pl-4 customP">{{ paragraphText }}</p> -->
+      </div>
+      <img class="w-44 h-12 pr-4"
+     :src="logo" alt="Logo" />
+  </div>
+</template>
